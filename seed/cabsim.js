6f0881de6890f4539ea100c0799c10e1af022697
Vincent Vullo <vincevullo@gmail.com>
2023-06-10 09:04:20 -0400

    updated cabsim seed file

--- seed/cabsim.js
+++ seed/cabsim.js
@@ -5,13 +5,13 @@ const main = async () => {
     const cabsims = [
         {
             name: "SWRL 8X10",
-            mic: "Dynamic 52",
-            image: "placeholder"
+            mic: "Dynamic 52, Position 2",
+            image: "https://ibb.co/5ncmWtR"
         },
         {
             name: "Darkglass 4X12",
-            mic: "Dynamic 52",
-            image: "placeholder"
+            mic: "Dynamic 52, Position 2",
+            image: "placehttps://ibb.co/LYHT9q3holder"
         }
     ]
     //await Cabsim.deleteMany()
