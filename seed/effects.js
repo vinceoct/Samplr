b63b13361820c3f16a2a67318b962d577e0c9bc1
Vincent Vullo <vincevullo@gmail.com>
2023-06-10 09:02:54 -0400

    updated effects seed file

--- seed/effects.js
+++ seed/effects.js
@@ -6,26 +6,20 @@ const main = async () => {
         {
             name: "Darkglass B3K", 
             description: "A defined and powerful overdrive, with a simple layout, it sets no boundaries or rules to unleashing the desired tone.",
-            settings: "Blend: 3 o'clock, Level: 9 o'clock, Tone: 12 o'clock, Drive: 2 o'clock",
-            image: "placeholder",
+            settings: "Blend, Level, Tone, Drive",
+            image: "https://ibb.co/y5JD0Jw",
         },
         {
             name: "Leo Bass", 
             description: "Clean American tube preamp with 3-band equalizer and volume controls",
-            settings: "Volume: 6, Master: 5, Treble: 6.5, Middle: 6.5, Bass: 6.5",
-            image: "placeholder",
+            settings: "Volume, Master, Treble, Middle, Bass",
+            image: "https://ibb.co/8xD7JQ2",
         },
         {
             name: "Sublime Octaver", 
             description: "A modern monophonic octaver with one octave down.",
-            settings: "Octave Level: o'clock, Clean Level: o'clock, Filter Cutoff: o'clock",
-            image: "placeholder",
-        },
-        {
-            name: "Hall Reverb", 
-            description: "Hall Reverb. Add massive space to your sound",
-            settings: "Dry: o'clock, Wet: o'clock, Decay: o'clock, Damping: 2 o'clock, Lo Cut: o'clock, Hi Cut: o'clock.",
-            image: "placeholder",
+            settings: "Octave Level, Clean Level, Filter Cutoff",
+            image: "https://ibb.co/rd7xLp2",
         },
     ]
     await Effect.deleteMany()
