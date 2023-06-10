a2f9d9d585412d777d74ca0f5b8b78cca41916aa
Vincent Vullo <vincevullo@gmail.com>
2023-06-10 15:31:45 -0400

    added getAudioById controller/router

--- controllers/audioController.js
+++ controllers/audioController.js
@@ -9,7 +9,19 @@ const getAllAudio = async (req, res) => {
         return res.status(500).send(error.message)
     }
 }
-
+const getAudioById = async (req, res) => {
+    try {
+        const { id } = req.params
+        const audio = await Audio.findById(id)
+        if(audio) {
+            return res.status(200).json({ audio })
+        }
+        return res.status(404).send('audio does not exist')
+    }catch (e){
+        return res.status(500).send(e.message)
+    }
+}
 module.exports = {
-    getAllAudio
+    getAllAudio,
+    getAudioById
 }
--- routes/audioRouter.js
+++ routes/audioRouter.js
@@ -2,5 +2,6 @@ const Router = require(`express`).Router()
 const controller = require(`../controllers/audioController`)
 
 Router.get('/', controller.getAllAudio)
+Router.get('/:id', controller.getAudioById)
 
 module.exports = Router
